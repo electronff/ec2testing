2edda4d5ff0a8cdd93f3ea408b7804af4c5a96a1
electronff <aminumuritala@gmail.com>
2021-10-28 23:04:18 -0500

    testing

--- main.tf
+++ main.tf
@@ -108,13 +108,13 @@ resource "aws_instance" "nodejs1" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg-nodejs-instance.id]
-  key_name               = var.ssh_key_name
+  // key_name               = var.ssh_key_name
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
-    private_key = file(var.private_key_path)
+    // private_key = file(var.private_key_path)
   }
 }
 
